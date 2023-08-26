4616ec3712ce53d17455e380d177a972aff6bcf6
Joulence <4erviken@gmail.com>
2023-08-26 13:54:27 +0300

    Created deposite and contract ampunts component

---
+++ src/components/InputDataAmount.js
@@ -0,0 +1,11 @@
+function InputDataAmount() {
+  return (
+    <div>
+      <div>input deposite</div>
+      <div>open contract price</div>
+      <div>close contract price</div>
+    </div>
+  );
+}
+
+export default InputDataAmount;
